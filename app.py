4e245f4efacabc8b2e060f62ed89e95c8a26ec30
chelseyong <chelseyong@gmail.com>
2018-10-07 20:54:58 +0800

    New features: loan out item, bid item, view user_profile + UI changes

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from config import conn_string
-from flask import Flask, render_template, request, redirect, send_from_directory, session
+from flask import Flask, render_template, request, redirect, send_from_directory, session, g
 from modules.users import user_module, get_current_user
+from modules.items import item_module
 import os
 import psycopg2
 
@@ -8,7 +9,14 @@ app = Flask(__name__)
 app.secret_key = os.urandom(12)
 #add your modules as blueprints here:
 app.register_blueprint(user_module)
+app.register_blueprint(item_module)
 
+@app.before_request
+def before_request():
+	if 'user' in session:
+		g.user = get_current_user()
+	else:
+		g.user = None
 
 @app.route("/")
 def index():
